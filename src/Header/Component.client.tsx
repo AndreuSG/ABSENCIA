c490baa2f2f68576b780d452b2f1be74e9f3156b
Xavi Rubio <ravirubio2621@gmail.com>
2025-06-16 13:32:59 +0200

    fix: delete extra logo from header

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -31,9 +31,6 @@ export const HeaderClient: React.FC<HeaderClientProps>= ({navItems}) => {
   return (
     <div className="container relative z-20" {...(theme ? { 'data-theme': theme } : {})}>
       <div className="py-8 flex items-center justify-between animate-in fade-in duration-700">
-        <Link href="/">
-          <Logo />
-        </Link>
         <HeaderNav navItems={navItems}/>
       </div>
     </div>
